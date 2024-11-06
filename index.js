805db91dc61d6337777a8419dd0db6e4c35d69c5
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-11-06 17:19:14 +0530

    second version

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log('v1') 
-console.log('v1') 
+console.log('v2') 
+
 
 //git init => to initialize the git in the current folder
 
 //git status
+
+
+//working directory : current folder that you are working in or you have intialize the git .
+
+//repo :
+
+//staging area : 
+//local repo  :
+//remote repo :
+
+//commit =>  version 
